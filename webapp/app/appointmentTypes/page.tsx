7785c619edf9b9a1e9feccf0a867f57cb59abbe8
Ismael Lares <ismael.lardom@gmail.com>
2026-09-12 11:55:25 -0700

    crear proyecto y primeras rutas

---
+++ webapp/app/appointmentTypes/page.tsx
@@ -0,0 +1,10 @@
+export default function AppointmentTypesPage() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-4xl font-bold mb-4">Appointment Types Page</h1>
+      <p className="text-lg text-gray-600">
+        This is the appointment types page of the application.
+      </p>
+    </div>
+  );
+}
---
+++ webapp/app/appointments/page.tsx
@@ -0,0 +1,10 @@
+export default function AppointmentsPage() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-4xl font-bold mb-4">Appointments Page</h1>
+      <p className="text-lg text-gray-600">
+        This is the appointments page of the application.
+      </p>
+    </div>
+  );
+}
