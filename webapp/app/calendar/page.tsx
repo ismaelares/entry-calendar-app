37ce9054841138e40a859b85b5c9c07d643e2b0a
Ismael Lares <ismael.lardom@gmail.com>
2026-09-13 07:13:44 -0700

    add calendar view

--- webapp/app/calendar/page.tsx
+++ webapp/app/calendar/page.tsx
@@ -1,10 +1,22 @@
-export default function CalendarPage() {
+import { appointmentsService } from "../services/appointments.service";
+import { appointmentTypesService } from "../services/appointmentsTypes.service";
+
+import Calendar from "../components/calendar/Calendar";
+
+export default async function CalendarPage() {
+  const [appointments, appointmentTypes] = await Promise.all([
+    appointmentsService.getAll(),
+    appointmentTypesService.getAll(),
+  ]);
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1 className="text-4xl font-bold mb-4">Calendar Page</h1>
-      <p className="text-lg text-gray-600">
-        This is the calendar page of the application.
-      </p>
-    </div>
+    <main className="min-h-screen p-6">
+      <div className="mx-auto max-w-7xl">
+        <Calendar
+          appointments={appointments}
+          appointmentTypes={appointmentTypes}
+        />
+      </div>
+    </main>
   );
 }
