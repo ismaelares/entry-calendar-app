a4ce9dc4adab63709731c3ec07cb3cfb89158524
Ismael Lares <ismael.lardom@gmail.com>
2026-09-12 19:09:18 -0700

    add new menu option, add styles

--- webapp/app/data/menu.ts
+++ webapp/app/data/menu.ts
@@ -1,7 +1,8 @@
 const navigationMenu = [
-  { id: 1, name: "Home", href: "/appointments" },
+  { id: 1, name: "Home", href: "/" },
   { id: 2, name: "Calendar", href: "/calendar" },
-  { id: 3, name: "Appointments Types", href: "/appointmentTypes" },
+  { id: 3, name: "Appointments", href: "/appointments" },
+  { id: 4, name: "Appointments Types", href: "/appointmentTypes" },
 ];
 
 export default navigationMenu;
